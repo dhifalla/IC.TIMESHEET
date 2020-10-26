0c5df98524845731ae8da5c92c0b7df18eb601f1
developper developper <bassem.trabelsi.1@esprit.tn>
2020-10-26 02:44:50 +0100

    entities

---
+++ src/main/java/tn/esprit/spring/TimesheetApplication.java
@@ -0,0 +1,19 @@
+package tn.esprit.spring;
+
+import java.util.EnumSet;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class TimesheetApplication {
+
+	public static void main(String[] args) {SpringApplication.run(TimesheetApplication.class, args);}
+
+
+ 
+}
